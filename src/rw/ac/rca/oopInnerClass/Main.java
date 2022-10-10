d3e182cd7911b8620c483e88bd46108bc6c0512c
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2022-10-10 09:18:05 +0200

    Static inner class

--- src/rw/ac/rca/oopInnerClass/Main.java
+++ src/rw/ac/rca/oopInnerClass/Main.java
@@ -5,6 +5,9 @@ public class Main {
         OuterClass myOuter = new OuterClass();
         OuterClass.InnerClass myInner = myOuter.new InnerClass();
 
+        StaticsOuter.Inner my2Inner = new StaticsOuter.Inner();
+
         System.out.println(myOuter.x + myInner.y);
+        System.out.println(my2Inner.a);
     }
 }
---
+++ src/rw/ac/rca/oopInnerClass/StaticsOuter.java
@@ -0,0 +1,9 @@
+package rw.ac.rca.oopInnerClass;
+
+public class StaticsOuter {
+    int z = 1000;
+
+    static class Inner {
+        int a = 100000;
+    }
+}
