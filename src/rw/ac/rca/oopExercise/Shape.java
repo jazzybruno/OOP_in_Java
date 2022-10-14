7a5a65792a4e2619e0d30120c22c31616856463d
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2022-10-14 12:33:53 +0200

    Adde exercise on the oop in java

---
+++ src/rw/ac/rca/oopExercise/Shape.java
@@ -0,0 +1,18 @@
+package rw.ac.rca.oopExercise;
+
+public class Shape {
+    private int color;
+    private float area;
+
+    public void getColor(){
+        System.out.println(color);
+    }
+
+    public int getArea() { return color; };
+
+    public void setArea(int area) { this.area = area; }
+
+    public void setColor(int color){
+        this.color = color;
+    }
+}
